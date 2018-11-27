da8421e0dd043ce76c22f64796c6a38110f12ab3
jian.chen@sohan.hk <jian.chen@sohan.hk>
2018-11-27 10:44:55 +0800

    其他包引入该工程，不需要将配置文件复制过去

--- scheduling/src/main/java/com/example/springboot/scheduling/service/DifferentNameForInterfaceImpl.java
+++ scheduling/src/main/java/com/example/springboot/scheduling/service/DifferentNameForInterfaceImpl.java
@@ -1,5 +1,7 @@
 package com.example.springboot.scheduling.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,9 +19,14 @@ import org.springframework.stereotype.Service;
 /**
  * 接口和实现类的名字一样，一样通过Autowired注入,详细见{@link com.example.springboot.properties.web.UserController}
  */
+@PropertySource(value = "classpath:test.properties") //自己命名的配置文件，需要指定
 public class DifferentNameForInterfaceImpl implements ITest{
+
+    @Value("${com.test.property}")
+    private String testString;
+
     @Override
     public String getString() {
-        return "yeyeyeyeeyeyeeyeyey";
+        return "yeyeyeyeeyeyeeyeyey" + testString;
     }
 }
