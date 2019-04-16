f1046bb66752d0d35ccbbfd5dc7eae328f0b113c
jian.chen@sohan.hk <jian.chen@sohan.hk>
2019-04-16 14:16:55 +0800

    优化执行

--- properties/src/main/java/com/example/springboot/properties/Thread/CallJasperThreadAspect.java
+++ properties/src/main/java/com/example/springboot/properties/Thread/CallJasperThreadAspect.java
@@ -3,6 +3,8 @@ package com.example.springboot.properties.Thread;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -19,8 +21,11 @@ import java.util.concurrent.CountDownLatch;
 @Component
 public class CallJasperThreadAspect {
 
-    private ThreadLocal<Long> startTime = new ThreadLocal<>(); // 开始时间
-    private ThreadLocal<Long> endTime = new ThreadLocal<>(); // 结束时间
+
+    private static final Logger logger = LoggerFactory.getLogger(CallJasperThreadAspect.class);
+    // 开始时间
+    private ThreadLocal<Long> startTime = new ThreadLocal<>();
+
 
     @Pointcut("@annotation(com.example.springboot.properties.Thread.CallJasperThreadManager)")
     public void annotationPointCut() {
@@ -35,30 +40,34 @@ public class CallJasperThreadAspect {
             ArrayBlockingQueue<CountDownLatch> queue = CallJasper.queue;
             queue.put(countDownLatch);
             countDownLatch.await();
-            startTime.set(System.currentTimeMillis());// 记录方法开始执行的时间
+            // 记录方法开始执行的时间
+            startTime.set(System.currentTimeMillis());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
     }
-    @Around("annotationPointCut()")
-    public Object advice(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object retmsg = joinPoint.proceed();
-        endTime.set(System.currentTimeMillis());// 记录方法执行完成的时间
-        if (endTime.get() != null && startTime.get() != null) {
-            long time = endTime.get() - startTime.get();
-            System.out.println("执行时间：" + time);
-        }
-        CountDownLatch countDownLatch = CallJasper.queue.poll();
-        if (countDownLatch != null) {
-            countDownLatch.countDown();
-        }
-        return retmsg;
-    }
 
     @After("annotationPointCut()")
     public void after() {
+        // 记录方法执行完成的时间
+        try {
+            long endTime = System.currentTimeMillis();
+            if (startTime.get() != null) {
+                long time = endTime - startTime.get();
+                if (time < 200) {
+                    Thread.sleep(200 - time);
+                }
+                time = System.currentTimeMillis() - startTime.get();
+                System.out.println("执行时间：" + time);
+            }
+            CountDownLatch countDownLatch = CallJasper.queue.poll();
+            if (countDownLatch != null) {
+                countDownLatch.countDown();
+            }
+        } catch (InterruptedException e) {
+            logger.error("线程睡眠失败", e);
+        }
         startTime.remove();
-        endTime.remove();
     }
 }
