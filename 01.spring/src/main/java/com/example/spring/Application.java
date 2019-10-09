8281f5c9df51d0501e9bdc8c92c434d2ffcb1b89
jian.chen@sohan.hk <jian.chen@sohan.hk>
2019-10-09 17:38:29 +0800

    添加新项目

---
+++ 01.spring/src/main/java/com/example/spring/Application.java
@@ -0,0 +1,16 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring组件注册的学习
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
