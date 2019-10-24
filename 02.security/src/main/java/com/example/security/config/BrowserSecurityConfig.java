16cab0ae42736013fdead96250c42be5250682dd
jian.chen@sohan.hk <jian.chen@sohan.hk>
2019-10-24 17:53:10 +0800

    拦截了解一下

---
+++ 02.security/src/main/java/com/example/security/config/BrowserSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http.formLogin() //  HTTP页面表单方式，有提示错误，会跳转页面 （默认）
+        http.httpBasic() // HTTP Basic方式 弹窗模式，不会跳转页面
+                .and()
+                .authorizeRequests() // 授权配置
+                .anyRequest()  // 所有请求
+                .authenticated(); // 都需要认证
+    }
+}
