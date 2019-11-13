e28652215112d39b2197a58f1cda1fdf9cef47f2
jian.chen@sohan.hk <jian.chen@sohan.hk>
2019-11-13 16:26:48 +0800

    未登陆处理器

--- 02.security/src/main/java/com/example/security/config/BrowserSecurityConfig.java
+++ 02.security/src/main/java/com/example/security/config/BrowserSecurityConfig.java
@@ -13,11 +13,13 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin() //  HTTP页面表单方式，有提示错误，会跳转页面 （默认）
                 //http.httpBasic() // HTTP Basic方式 弹窗模式，不会跳转页面
-                .loginPage("/login.html") // 登录跳转 URL
-                .loginProcessingUrl("/login") //处理表单登录 URL
+                //.loginPage("/login.html") // 登录跳转 URL
+                //动态处理，接口访问，返回错误信息，页面访问，跳转到登录页
+                .loginPage("/authentication/require")
+                .loginProcessingUrl("/login") //处理表单登录 URL,处理登陆验证的url
                 .and()
                 .authorizeRequests() // 授权配置
-                .antMatchers("/login.html").permitAll() //登录跳转 URL 无需认证
+                .antMatchers("/authentication/require","/login.html").permitAll() //登录跳转 URL 无需认证
                 .anyRequest()  // 所有请求
                 .authenticated()// 都需要认证
                 .and().csrf().disable()
---
+++ 02.security/src/main/java/com/example/security/controller/BrowserSecurityController.java
@@ -0,0 +1,44 @@
+package com.example.security.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Program:     com.example.security.controller <br/>
+ * ClassName:   BrowserSecurityController <br/>
+ * Description: 未登陆处理器 <br/>
+ * Author:      陈建 <br/>
+ * Create:      2019-11-13 15:30 <br/>
+ */
+@RestController
+public class BrowserSecurityController {
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @GetMapping("/authentication/require")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        if (savedRequest != null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")){
+                redirectStrategy.sendRedirect(request, response, "/login.html");
+            }
+        }
+        return "访问的资源需要身份认证！";
+    }
+}
